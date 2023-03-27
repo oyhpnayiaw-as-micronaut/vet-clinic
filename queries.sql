4a05f1d5025a667f762774644646f6837e56a5d3
EBrain <ebrain.ebrain@gmail.com>
2023-03-28 00:46:34 +0630

    Fix query

--- queries.sql
+++ queries.sql
@@ -55,7 +55,7 @@ SELECT
 FROM
   animals AS ani
 WHERE
-  ani.name = 'Gabumon';
+  ani.name != 'Gabumon';
 
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT
